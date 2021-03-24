79f0ae7a9c3098f6e4612663c05a376fec911191
smilejakdu <ash982416@gmail.com>
2021-03-25 05:44:33 +0900

    corona

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+// rafce
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>COVID-19</h1>
+      <select>
+        <option>국내</option>
+        <option>세계</option>
+        <option></option>
+      </select>
+    </header>
+  );
+};
+
+export default Header;
